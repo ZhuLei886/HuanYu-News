d623f13bd4b38cfa180fc8e3b121f927a21a8da3
ZhuLei886 <38902234+ZhuLei886@users.noreply.github.com>
2019-06-26 14:31:36 +0800

    [news]控制器

---
+++ src/main/java/com/huanyunews/news/interceptor/PassportInterceptor.java
@@ -0,0 +1,31 @@
+package com.huanyunews.news.interceptor;
+
+
+import com.huanyunews.news.redis.JedisService;
+import com.huanyunews.news.util.RedisKeyUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+
+ */
+@Component
+public class PassportInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private JedisService jedisService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+
+
+        String uri = httpServletRequest.getServletPath();
+        String uriKey = RedisKeyUtil.getClickCountKey(uri);
+        jedisService.incr(uriKey);
+        return true;
+    }
+}
